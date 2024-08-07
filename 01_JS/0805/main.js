0c4613383f53c17adc925315c01a59d5c9aaa515
sunglitter <kds0284@gmail.com>
2024-08-07 23:21:19 +0900

    fix: 기존 파일 수정(240806)

--- 01_JS/0805/main.js
+++ 01_JS/0805/main.js
@@ -159,39 +159,85 @@ import axios from 'axios'
 
 // 영화 검색창을 추가해서 연동시키는 기능까지 포함된 것을 어떻게 구현할지 시도해보기 => 하긴 했음
 
+// 강사님 방식
+
+function getMovies(title = 'hello') {
+    axios
+    .get(`https://www.omdbapi.com/?apikey=bcb8bfc2&s=${title}`)
+    .then((response) => {
+        console.log(response);
+        const results = document.getElementById('results');
+        results.innerHTML = '';
+
+        if(response.data.Search && response.data.Search.length > 0){
+            const movies = response.data.Search.slice(0, 10);
+            movies.forEach((movie) => {
+                const movieEl = document.createElement('div');
+                const h1El = document.createElement('h1');
+                const imgEl = document.createElement('img');
+                const yearEl = document.createElement('p');
+                const typeEl = document.createElement('p');
+
+                h1El.textContent = movie.Title;
+                imgEl.src = movie.Poster;
+                yearEl.textContent = movie.Year;
+                typeEl.textContent = movie.Type;
+
+                movieEl.appendChild(h1El);
+                movieEl.appendChild(imgEl);
+                movieEl.appendChild(yearEl);
+                movieEl.appendChild(typeEl);
+                results.appendChild(movieEl);
+            })
+
+        }else{
+            alert('No results found');
+        }
+    })
+}
+getMovies();
+document.getElementById('searchButton').addEventListener('click', () => {
+    const searchInput = document.getElementById('searchInput').value;
+    if(searchInput.trim() !== ''){
+        getMovies(searchInput);
+    }else{
+        alert('영화 제목을 입력해주세요!');
+    }
+})
+
 // 자전거 지도 만들기
-const API_KEY = "";
-async function getData() {
-    const url = `http://openapi.seoul.go.kr:8088/${API_KEY}/json/bikeList/1/10/`;
-    const response = await fetch(url);
-    const data = await response.json();
-    const locations = data.rentBikeStatus.row.map(spot => [spot.stationName, spot.stationLatitude,
-        spot.stationLongitude, spot.parkingBikeTotCnt])
-    console.log(data);
-    console.log("locations", locations)
-    drawMap(locations);
-}
-getData();
+// const API_KEY = "";
+// async function getData() {
+//     const url = `http://openapi.seoul.go.kr:8088/${API_KEY}/json/bikeList/1/10/`;
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     const locations = data.rentBikeStatus.row.map(spot => [spot.stationName, spot.stationLatitude,
+//         spot.stationLongitude, spot.parkingBikeTotCnt])
+//     console.log(data);
+//     console.log("locations", locations)
+//     drawMap(locations);
+// }
+// getData();
 
-function drawMap(locations){
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 15,
-        center: new google.maps.LatLng(locations[0][1], locations[0][2]),
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-    });
+// function drawMap(locations){
+//     const map = new google.maps.Map(document.getElementById("map"), {
+//         zoom: 15,
+//         center: new google.maps.LatLng(locations[0][1], locations[0][2]),
+//         mapTypeId: google.maps.MapTypeId.ROADMAP,
+//     });
 
-    const infowindow = new google.maps.InfoWindow();
-    locations.forEach((location, i) => {
-        const marker = new google.maps.Marker({
-            position: new google.maps.LatLng(location[1], location[2]),
-            map: map,
-            title: location[0]
-        });
+//     const infowindow = new google.maps.InfoWindow();
+//     locations.forEach((location, i) => {
+//         const marker = new google.maps.Marker({
+//             position: new google.maps.LatLng(location[1], location[2]),
+//             map: map,
+//             title: location[0]
+//         });
         
-        marker.addListener("click", () => {
-            infowindow.setContent(location[3]);
-            infowindow.open(map, marker);
-        });
-    });
+//         marker.addListener("click", () => {
+//             infowindow.setContent(location[3]);
+//             infowindow.open(map, marker);
+//         });
+//     });
 
-}
+// }
